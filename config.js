2525cf978fb92d82a9c890e2f6a4ba27bfa8e48f
Cocdas <138351304+Cocdas@users.noreply.github.com>
2024-11-04 01:59:43 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ||"3ColWC5Z#rRtwt-pUZJMWcKtT70VpHJQHeGWoIsMbqxfn6_LgdTg",
+SESSION_ID: process.env.SESSION_ID ||"52cVVZja#hx40e8wMF4UlMQk1BJGV-hbC7vxg2CycQVO9ooOa-xY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/YhYStZ8/IMG-20240923-WA0006.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HII. I AM QUEEN_THARU_V➄ WHATSAPP BOT OWNER BY CHAMI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "ture",
