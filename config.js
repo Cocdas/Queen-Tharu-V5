de824c592e413300a21e35a703357af97a690cec
Cocdas <138351304+Cocdas@users.noreply.github.com>
2024-11-05 08:07:22 -0800

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID ||"52cVVZja#hx40e8wMF4UlMQk1BJGV-hbC7vxg2CycQ
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/YhYStZ8/IMG-20240923-WA0006.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HII. I AM QUEEN_THARU_V➄ WHATSAPP BOT OWNER BY CHAMI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "ture",
+ OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com  
 };
